1552298abb2289ea375eb24f6a8676b4418b15d1
Zaid Bouslikin | MX3D <Zaid@MX3D.local>
2023-08-24 14:54:31 +0200

    clean

--- vite.renderer.config.mjs
+++ vite.renderer.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import renderer from 'vite-plugin-electron-renderer';
 
-console.log
 // https://vitejs.dev/config
 export default defineConfig({
     resolve: {
